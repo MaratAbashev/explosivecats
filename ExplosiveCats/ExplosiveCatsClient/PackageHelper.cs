5988806cb1ad5520e92d7dcca0d25a01537f1f9a
MaratAbashev <maratabashev@yandex.ru>
2025-01-31 04:53:48 +0300

    написан базовый клиент без логики

---
+++ ExplosiveCats/ExplosiveCatsClient/PackageHelper.cs
@@ -0,0 +1,78 @@
+﻿using ExplosiveCatsEnums;
+
+namespace ExplosiveCatsClient;
+
+public static class PackageHelper
+{
+    public const int MaxPacketSize = 15;
+    public const int MaxBasePacketBytes = 5;
+    public const int MaxFreeBytes = MaxPacketSize - MaxBasePacketBytes;
+
+    public const int Action = 4;
+    public const int PlayerId = 5;
+    public const int CardByte = 6;
+    public const int ExplosiveCatInsertionId = 6;
+    public const int AnotherPlayerId = 7;
+    public const int SecondCardType = 7;
+    public const int PlayersCount = 15;
+
+    public static readonly byte[] BasePackage =
+    {
+        0x31, 0x34, 0x38, 0x38
+    };
+
+    public static bool IsQueryValid(byte[] buffer) =>
+        IsCorrectProtocol(buffer) && HasAction(buffer);
+
+    public static bool IsCorrectProtocol(byte[] buffer) =>
+        buffer[..4].SequenceEqual(BasePackage);
+
+    public static bool HasAction(byte[] buffer) =>
+        Enum.GetValues<ServerActionType>().Any(a => (byte)a == buffer[Action]);
+    
+    public static byte[] CreateJoinPackage() => 
+        new PackageBuilder(MaxBasePacketBytes)
+            .WithCommand(ActionType.Join)
+            .Build();
+
+    public static byte[] CreateReadyPackage(byte playerId) =>
+        new PackageBuilder(MaxBasePacketBytes + 1)
+            .WithCommand(ActionType.Ready)
+            .WithPlayerId(playerId)
+            .Build();
+    
+    public static byte[] CreatePlayCardPackage(byte playerId, Card card) =>
+        new PackageBuilder(MaxBasePacketBytes + 2)
+            .WithCommand(ActionType.PlayCard)
+            .WithPlayerId(playerId)
+            .WithCard(card)
+            .Build();
+    public static byte[] CreateTakeCardPackage(byte playerId) =>
+        new PackageBuilder(MaxBasePacketBytes + 1)
+            .WithCommand(ActionType.TakeCard)
+            .WithPlayerId(playerId)
+            .Build();
+
+    public static byte[] CreateGiveCardPackage(byte playerId, Card card, byte anotherplayerId) =>
+        new PackageBuilder(MaxBasePacketBytes + 3)
+            .WithCommand(ActionType.GiveCard)
+            .WithPlayerId(playerId)
+            .WithCard(card)
+            .WithAnotherPlayerId(anotherplayerId)
+            .Build();
+
+    public static byte[] CreatePlayDefusePackage(byte playerId,Card card, byte insertionId) =>
+        new PackageBuilder(MaxBasePacketBytes + 3)
+            .WithCommand(ActionType.PlayDefuse)
+            .WithPlayerId(playerId)
+            .WithCard(card)
+            .WithExplosiveCatInsertionId(insertionId)
+            .Build();
+    
+    public static byte[] CreatePlayNopePackage(byte playerId, Card card) =>
+        new PackageBuilder(MaxBasePacketBytes + 2)
+            .WithCommand(ActionType.PlayNope)
+            .WithPlayerId(playerId)
+            .WithCard(card)
+            .Build();
+}
--- ExplosiveCats/ExplosiveCatsClient/Program.cs
+++ ExplosiveCats/ExplosiveCatsClient/Program.cs
@@ -1,56 +1,19 @@
 ﻿using System.Net.Sockets;
 using System.Text;
-using TcpChatServer;
-using static TcpChatServer.PackageHelper;
-using System.Collections.Generic;
+using ExplosiveCatsClient;
+using static ExplosiveCatsClient.PackageHelper;
+using PackageBuilder = ExplosiveCatsClient.PackageBuilder;
+using PackageHelper = ExplosiveCatsClient.PackageHelper;
 
-var clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+var client = new Client();
 try
 {
-    await clientSocket.ConnectAsync("localhost", 5000);
-    Console.WriteLine("Кто ты, воин?");
-    var name = Console.ReadLine();
-
-    var content = Encoding.UTF8.GetBytes(name!);
-
-    var helloPackage = CreatePackage(content, SupportCommand.Hello, FullnessPackage.Full, QueryType.Request);
-
-    await clientSocket.SendAsync(new ArraySegment<byte>(helloPackage), SocketFlags.None);
-
-    do
-    {
-        var response = await GetResponse(clientSocket);
-
-        Console.WriteLine(response);
-
-        Console.WriteLine("Введите команду");
-        var command = Console.ReadLine();
-        if (command != "q")
-        {
-            var message = Console.ReadLine();
-            var packages = GetPackagesByMessage(
-                Encoding.UTF8.GetBytes(message!), SupportCommand.Say, QueryType.Request);
-
-            foreach (var package in packages)
-            {
-                await clientSocket.SendAsync(package, SocketFlags.None);
-            }
-        }
-        else
-        {
-            var byePackage = new PackageBuilder(0)
-                .WithCommand(SupportCommand.Bye)
-                .WithFullness(FullnessPackage.Full)
-                .WithQueryType(QueryType.Request)
-                .Build();
-
-            await clientSocket.SendAsync(byePackage, SocketFlags.None);
-            Console.WriteLine("bye bye!");
-
-            break;
-        }
-
-    } while (clientSocket.Connected);
+    var playerId = await client.StartClient("localhost", 5000);
+    await client.GetReady(playerId);
+    var result = await client.GetResponse();
+    Console.WriteLine($"Действие сервера:{result.Action}");
+    Console.WriteLine($"Игроков всего:{result.PlayerCount}");
+    Console.WriteLine("Карты игрока:" + string.Join(' ',result.Cards!.Select(card => card.CardType.ToString())));
 }
 catch (Exception ex)
 {
@@ -58,26 +21,5 @@ catch (Exception ex)
 }
 finally
 {
-    await clientSocket.DisconnectAsync(false);
-    clientSocket.Dispose();
-}
-
-async Task<string> GetResponse(Socket socket)
-{
-    var buffer = new byte[MaxPacketSize];
-    var responseContent = new List<byte>();
-    do
-    {
-        var contentLength = await socket.ReceiveAsync(buffer, SocketFlags.None);
-        
-        if (!IsResponse(buffer[Query]) || !IsSay(buffer[PackageHelper.Action]))
-        {
-            return "Получили неизвестный ответ от сервера!";
-        }
-
-        responseContent.AddRange(GetContent(buffer, contentLength));
-
-    } while (!IsFull(buffer[Fullness]));
-
-    return Encoding.UTF8.GetString(responseContent.ToArray());
+    await client.CloseClient();
 }
